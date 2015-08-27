ea2d61fe2b8971c9492e444e626c9dfb168bc955
wenmengxia <wenmengxia@baidu.com>
2015-08-27 17:22:47 +0800

    'first-page'

--- index.js
+++ index.js
@@ -3,9 +3,34 @@ var app = express();
 
 app.set('views', './views');
 app.set('view engine', 'jade');
+app.use(require('less-middleware')(__dirname + '/public'));
 app.use(express.static('bower_components'));
+app.use(express.static('public'));
 app.get('/',function(req,res) {
-    res.render('index', {title:'首页', message:'Hello Word!'});
+    res.render('index', {title:'首页', 
+                        message:'Hello Word!',
+                        user:{
+                            name:'叮叮'
+                        },
+                        gameLists:[
+                            {
+                                id: 1,
+                                name: '渺茫'
+                            },
+                            {
+                                id: 2,
+                                name: '那你慢慢'
+                            },
+                            {
+                                id: 3,
+                                name: 'xxx'
+                            },
+                            {
+                                id: 4,
+                                name: 'server'
+                            }
+                        ]
+                    });
 });
 
 var server = app.listen(3000, function() {
